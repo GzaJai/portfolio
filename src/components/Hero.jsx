2709bf6c0d084532527b6ed64b1c3e357919e0cf
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2024-03-20 20:00:44 -0300

    fixing width for small decices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
             I'm Gonzalo Jaime, a software development student.  
           </p>
         </div>
-        <img className='w-[300px] bg-indigo-400 p-4 rounded-full' src="https://raw.githubusercontent.com/GzaJai/portfolio/main/public/images/yo-portfolio.png" alt="" />
+        <img className='bg-indigo-400 p-4 rounded-full' src="https://raw.githubusercontent.com/GzaJai/portfolio/main/public/images/yo-portfolio.png" alt="" />
     </>
   )
 }
