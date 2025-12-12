0c3e7166fca379d2cc3a449d64c70f2016bc7942
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-12-12 15:08:53 -0300

    Restyling header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-      <header className='mt-12 mb-8'>
+      <header className='pt-6 mb-8 sticky top-0 bg-[#d9d9d9]'>
         <div className='mx-10 flex justify-between items-center gap-20 max-sm:ml-[3rem] md:w-[45%] md:mx-auto '>
-          <h1 className='font-bold text-lg md:text-3xl'>Gonzalo Jaime</h1>
+          <a href='#' className='font-bold text-lg md:text-3xl'>Gonzalo Jaime</a>
           <nav className='flex list-none gap-6 text-xl font-bold max-sm:hidden'>
             <li>
               <a href="#about-me" className='hover:text-orange-500 duration-150'>
@@ -25,8 +25,8 @@ const Header = () => {
           </nav>
           <img className='w-[4rem] bg-indigo-400 p-1 rounded-full md:hidden' src="https://raw.githubusercontent.com/GzaJai/portfolio/main/public/images/yo-portfolio.png" alt="" />
         </div>
+        <hr className='w-[40%] mx-auto border-gray-600 max-sm:w-[80%] mt-8' />
         </header>
-        <hr className='w-[40%] mx-auto border-gray-600 max-sm:w-[80%]' />
     </>
   )
 }
