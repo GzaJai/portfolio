337a30b09e44db059b7568bfeeedf5d0b19f2cf1
Gonzalo Jaime <gjaimeguinazu@gmail.com>
2025-12-12 16:01:34 -0300

    Changing color and spacing

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,7 +6,7 @@ const ProjectCard = ({ title, desc, img, openModal }) => {
     <div
       onClick={openModal}
       className="
-        w-full max-w-xs bg-[#444] rounded-lg hover:bg-orange-500 hover:cursor-pointer duration-150 box-border p-5 flex flex-col items-center min-h-[280px]"
+        w-full max-w-xs bg-[#707070] rounded-lg hover:bg-orange-500 hover:cursor-pointer duration-150 box-border p-5 flex flex-col items-center justify-between min-h-[280px]"
     >
       <img
         className="w-[85%] rounded-md mb-4"
@@ -15,7 +15,7 @@ const ProjectCard = ({ title, desc, img, openModal }) => {
       />
 
       <h3 className="text-xl font-bold text-white text-center">{title}</h3>
-      <p className="text-white text-center text-sm">{desc}</p>
+      {/* <p className="text-white text-center text-sm">{desc}</p> */}
     </div>
   )
 }
